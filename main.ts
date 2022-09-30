ec8c2f38231983a351010ca311e4ab1e3c347651
Daniel Skysľak <danilesky17@gmail.com>
2022-09-30 09:59:00 +0200

    made a f.kin logic

--- main.ts
+++ main.ts
@@ -22,15 +22,25 @@ let level = [
 //This is initializator (main function)
 const init: any = (level: string[]) => {
   let i: number = 1;
+
+  //Coordinates marking END and START of the level
+  const YstartLevel = 1;
+  const YendLevel = level.length - 1;
+
+  const XstartLevel = 1;
+
   setInterval(async () => {
+    if (i === YstartLevel || i === YendLevel) {
+      i = 1;
+    }
     level[i] = level[i].substring(0, i) + "o" + level[i].substring(i + 1);
     console.log(level);
     level[i] = level[i].substring(0, i) + " " + level[i].substring(i + 1);
     setTimeout(() => {
       console.clear();
       i++;
-    }, 500);
-  }, 500);
+    }, 100);
+  }, 100);
 };
 
 init(level);
